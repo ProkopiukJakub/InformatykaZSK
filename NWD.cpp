65c4477bbb49493ff414eb0a1d4879a6ef9dd6da
ProkopiukJakub <99956054+ProkopiukJakub@users.noreply.github.com>
2022-03-24 09:03:52 +0100

    Add files via upload

---
+++ NWD.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+
+    cout << "Podaj pierwsza liczbe: ";
+    cin >> a;
+    cout << "Podaj druga liczbe: ";
+    cin >> b;
+
+    int iloczyn = a * b;
+
+    while (a != b)
+    {
+        if (a < b)
+        {
+            b = b - a;
+        }
+        else
+        {
+            a = a - b;
+        }
+        
+    
+    }
+    cout <<"Najwiekszy wspolny dzielnik: "<< b;
+
+
+}
---
+++ Nww.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+
+    cout << "Podaj pierwsza liczbe: ";
+    cin >> a;
+    cout << "Podaj druga liczbe: ";
+    cin >> b;
+
+    int iloczyn = a * b;
+    while (a != b)
+    {
+        if (a < b)
+        {
+            b = b - a;
+        }
+        else
+        {
+            a = a - b;
+        }
+
+
+    }
+    int nww = iloczyn / a;
+    cout << "Najmniejsza wspolna wielokrotnosc: " << nww;
+}
