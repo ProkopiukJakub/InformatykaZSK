e3d877be34b24f0c821c684ad12e691dd19cfe6c
ProkopiukJakub <99956054+ProkopiukJakub@users.noreply.github.com>
2022-04-21 09:11:09 +0200

    Add files via upload

---
+++ Ciag Fibonaciego.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int n = 0;
+	int f0 = 0;
+	int f1 = 1;
+	int f = 0;
+
+	cout << "Ile wyrazow ciagu wypisac: ";
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		if (i > 1) {
+			f = f1 + f0;
+			f0 = f1;
+			f1 = f;
+		}
+		else {
+			f = i;
+		}
+		cout << f << endl;
+	}
+
+
+
+
+
+
+
+
+
+
+}
